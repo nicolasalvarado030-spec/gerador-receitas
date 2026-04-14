5a780570ccb2f5b014920c67dce9a8221a10d248
nicolasalvarado030-spec <nicolasalvarado030@gmail.com>
2026-04-14 18:30:31 -0300

    Update dalle.js

--- api/dalle.js
+++ api/dalle.js
@@ -12,11 +12,10 @@ export default async function handler(req, res) {
         'Authorization': `Bearer ${process.env.OPENAI_KEY}`
       },
       body: JSON.stringify({
-        model: 'dall-e-3',
+        model: 'dall-e-2',
         prompt,
         n: 1,
-        size: '1024x1024',
-        quality: 'standard',
+        size: '512x512',
         response_format: 'b64_json'
       })
     });
